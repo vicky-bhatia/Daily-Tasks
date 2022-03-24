acd975e8262b6c1ebb255bcc7f9da862440880c6
vicky-bhatia <vicky.bhatia@qualitestgroup.com>
2022-03-24 13:16:37 +0530

    Second commit

---
+++ NDTV/src/test/java/pack1/FinalYaml.java
@@ -0,0 +1,32 @@
+package pack1;
+//package properties;
+//
+//public class FinalYaml {
+//	public First yd;
+//	public WeatherPage wp;
+//	
+//	public FinalYaml() {
+//
+//	}
+//
+////	public FinalYaml(First yd, WeatherPage wp) {
+////		this.yd = yd;
+////		this.wp = wp;
+////	}
+////	public String getMore_id() {
+////		return more_id;
+////	}
+////	public void setMore_id(String more_id) {
+////		this.more_id = more_id;
+////	}
+////	public String getWeather_xpath() {
+////		return weather_xpath;
+////	}
+////	public void setWeather_xpath(String weather_xpath) {
+////		this.weather_xpath = weather_xpath;
+////	}
+////	
+//
+//	
+//
+//}
---
+++ NDTV/src/test/java/pack1/First.java
@@ -0,0 +1,19 @@
+package pack1;
+
+public class First {
+	private String more_id;
+	private String weather_xpath;
+	
+	public String getMore_id() {
+		return more_id;
+	}
+	public void setMore_id(String more_id) {
+		this.more_id = more_id;
+	}
+	public String getWeather_xpath() {
+		return weather_xpath;
+	}
+	public void setWeather_xpath(String weather_xpath) {
+		this.weather_xpath = weather_xpath;
+	}
+}
